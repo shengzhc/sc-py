655117ef1a2d20b5bbed65bcd8326679b2f456a9
Shengzhe Chen <shengzhe.chen@gmail.com>
2020-12-14 23:30:39 -0800

    update formats

--- src/format.py
+++ src/format.py
@@ -8,6 +8,9 @@ print("left aligned with 10 spots: {}".format(format("Hello", "*<10s")))
 print("center aligned with 20 spots: {}".format(format("Hello", "*^20s")))
 
 print("=====Justify=====")
-print('<--{0:*>30}-->'.format('Python Programming'))
-print('<--{0:*<30}-->'.format('Python Programming'))
-print('<--{0:*^30}-->'.format('Python Programming'))
+print('<--{:*>30}-->'.format('Python Programming'))
+print('<--{:*<30}-->'.format('Python Programming'))
+print('<--{:*^30}-->'.format('Python Programming'))
+
+print("=====Number print as Binary=====")
+print('{0:b}'.format(10))
