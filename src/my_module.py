6d7ddd05a3fd2f5b32f1c74a38657b04a1d57a85
Shengzhe Chen <shengzhe.chen@gmail.com>
2020-12-21 01:52:55 -0800

    add testing module

---
+++ src/my_module.py
@@ -0,0 +1,10 @@
+import gen_expression as ge
+
+print("=====GE.AP Class=====")
+for ele in ge.AP(10, 2, 10):
+    print(ele)
+
+print("\n\n\n")
+print("=====GE.ap_yield=====")
+for ele in ge.ap_yield(10, 2, 5):
+    print(ele)
